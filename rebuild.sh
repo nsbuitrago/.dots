520a518d19d839015ee61342a1c9ac53576eb0df
nsbuitrago <nsb5@rice.edu>
2024-02-28 23:27:47 -0600

    add rebuild.sh script for easier rebuilding

---
+++ rebuild.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# get passed argument for the type of rebuild (either system or home)
+rebuild_type=$1
+
+if [ -z $rebuild_type ]; then
+  echo "This is a simple command line tool for rebuilding your NixOS. Please provide either system or home as an argument."
+  exit 1
+elif [ $rebuild_type == "system" ]; then
+	sudo nixos-rebuild switch --flake
+elif [ $rebuild_type == "home" ]; then
+	home-manager switch --flake '.?submodules=1'
+else
+	echo "Invalid rebuild type. Please provide either system or home"
+	exit 1
+fi
+
+
+
